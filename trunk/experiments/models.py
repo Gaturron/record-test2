263da32165c946073ff812075ebe41f2ecc18d6e
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2012-11-26 19:09:35 +0000

    experiments ABM Pictures

--- trunk/experiments/models.py
+++ trunk/experiments/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Word(models.Model):
-    enabled = models.BooleanField()
+    enabled = models.BooleanField(default=True)
     text = models.TextField()
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    amount = models.IntegerField(default=0)
 
 class Phrase(models.Model):
-    enabled = models.BooleanField()
+    enabled = models.BooleanField(default=True)
     text = models.TextField()
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    amount = models.IntegerField(default=0)
 
 class Picture(models.Model):
-    enabled = models.BooleanField()
+    enabled = models.BooleanField(default=True)
     image = models.FileField(upload_to='imagenesExperimentos')
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    amount = models.IntegerField(default=0)
