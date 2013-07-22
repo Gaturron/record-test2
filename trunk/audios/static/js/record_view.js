b5f72fd6ee1d09f633dc6c947fc86aac50751068
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2013-07-22 19:20:25 +0000

    Agrego record_view.js

---
+++ trunk/audios/static/js/record_view.js
@@ -0,0 +1,21 @@
+var view = {
+    'tabs': '#tabs',
+    'dialogModal': '#dialog-modal',
+    'runners': '.runner',
+    'nextProduct': '#next-product',
+    'prevProduct': '#previous-product',
+    'testContador': '#test-contador',
+    'dialogPopUp': '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-draggable.ui-resizable',
+    'spinners': '.spinner',
+    'meters': '.meter',
+    'status': '.status',
+    'record': '.record',
+    'stop': '.stop',
+    'play': '.play',
+    'saturation': '.saturation',
+    'wordexp': '.wordexp:visible',
+    'confirm': '#confirm',
+    'exit': '#exit',
+    'dialogConfirm': '#dialog-confirm',
+    'speakerId': '#speaker_id'
+};
