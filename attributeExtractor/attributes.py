14e4011f12d4ad468567bd460536c00ea8155d74
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2013-12-26 23:08:22 +0000

    calculo de atributos

--- attributeExtractor/attributes.py
+++ attributeExtractor/attributes.py
@@ -1,5 +1,15 @@
+from textgrid import TextGrid, Tier
+
 def duration(textgrid):
-    return textgrid.t_time
+    res = float(0)
+    for i, tier in enumerate(textgrid):
+        if tier.nameid == 'words':
+            for row in tier.simple_transcript:
+                if row[2] != 'sil' and row[2] != 'sp':
+                    res += float(row[1]) - float(row[0])
+                print row
+    print 'resultado: '+str(res)
+    return res
 
 def dummy(textgrid):
     return '8'
