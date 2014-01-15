1a2f6e726358aacf2dfce792d24d964c9e2524d9
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2014-01-15 19:32:55 +0000

    diccToArff

--- attributeExtractor/mfccExample.py
+++ attributeExtractor/mfccExample.py
@@ -1,10 +1,11 @@
 from features import mfcc
 from features import logfbank
 import scipy.io.wavfile as wav
+import scikits.talkbox.features as features
 
 wavFile = '/home/fernando/Tesis/Prosodylab-Aligner-master/data/bsas_u2_t44_a1.wav'
 (rate,sig) = wav.read(wavFile)
-mfcc_feat = mfcc(sig,rate)
 
-print mfcc_feat[1:3,:]
-print len(mfcc_feat)
+mfcc_feat = mfcc(sig,rate)
+print mfcc_feat
+print features.mfcc(sig, fs = rate)
