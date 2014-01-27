24e6bab65f172b71cc6b38782c9dd833d5e8d46c
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2014-01-27 19:19:22 +0000

    acousticExtractor

--- trunk/attributeExtractor/extract.py
+++ trunk/attributeExtractor/extract.py
@@ -1,14 +1,17 @@
 import alignmentFilter as agmntFilter
 import textgridExtractor as tgExtractor
+import acousticExtractor as acExtractor
 
 Path = '/home/fernando/Tesis/Prosodylab-Aligner-master/data1/'
 
 def extract():
 
-	pathList = agmntFilter.filter(Path)
-	print pathList
-	res = tgExtractor.textgridsToAtt(pathList)
-	print res
+    pathList = agmntFilter.filter(Path)
+    print pathList
+    #res = tgExtractor.textgridsToAtt(pathList)
+    #print res
+    res = acExtractor.extracts(pathList)
+    print res
 
 if __name__ == '__main__':
-	extract()
+    extract()
