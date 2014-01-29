282fd3b00db60c79056da490c581175b0130d7f0
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2014-01-29 00:12:47 +0000

    Primer implementacion accousticAttributes

--- trunk/attributeExtractor/acousticAttributes.py
+++ trunk/attributeExtractor/acousticAttributes.py
@@ -2,10 +2,11 @@ from textgrid import TextGrid, Tier
 import mfccExtractor as mfccExt
 import phraseToAccents as phToAcc
 import attributes as att
+import numpy as np
 import re
 
 # La 'c' antes de la 't' no suena
-def mfccKT(textgrid, mfcc):
+def _foundPattern(wordPattern, syllablePattern, textgrid, mfcc):
     accents = att._accents(textgrid)
 
     # buquemos si esta es la frase con 'CT'
@@ -14,7 +15,7 @@ def mfccKT(textgrid, mfcc):
     for i, tier in enumerate(textgrid):
         if tier.nameid == 'words':
             for row in tier.simple_transcript:
-                if re.search(r'.CT.', str(row[2])):
+                if re.search(wordPattern, str(row[2])):
                     interval['xmin'] = row[0]
                     interval['xmax'] = row[1]
 
@@ -27,7 +28,8 @@ def mfccKT(textgrid, mfcc):
             for row in tier.simple_transcript:
 
                 # guardo la letra previa sino es
-                if str(prevChar)+str(row[2]) == 'kt':
+                # if str(prevChar)+str(row[2]) == 'kt':
+                if re.search(syllablePattern, str(prevChar)+str(row[2])):
 
                     if interval['xmin'] < row[0] and row[1] < interval['xmax']:
                         interval1['xmin'] = row[0]
@@ -36,10 +38,35 @@ def mfccKT(textgrid, mfcc):
                     prevChar = row[2]
 
     if interval1:
-        print mfcc[float(interval1['xmin']):float(interval1['xmax'])]
         return mfcc[float(interval1['xmin']):float(interval1['xmax'])]
     else:
-        return False 
+        return False
+
+def mfccAverageKT(textgrid, mfcc):
+    mfccTemp = _foundPattern(r'.CT.', r'kt', textgrid, mfcc)
+    return np.average(mfccTemp, axis=0)
+
+def mfccMaxKT(textgrid, mfcc):
+    mfccTemp = _foundPattern(r'.CT.', r'kt', textgrid, mfcc)
+    return np.amax(mfccTemp, axis=0)
+
+def mfccMinKT(textgrid, mfcc):
+    mfccTemp = _foundPattern(r'.CT.', r'kt', textgrid, mfcc)
+    return np.amin(mfccTemp, axis=0)
+#=======================================================================
+def mfccAverageLL(textgrid, mfcc):
+    mfccTemp = _foundPattern(r'(.*)LL.', r'.Z', textgrid, mfcc)
+    return np.average(mfccTemp, axis=0)
+
+def mfccMaxLL(textgrid, mfcc):
+    mfccTemp = _foundPattern(r'(.*)LL.', r'.Z', textgrid, mfcc)
+    return np.amax(mfccTemp, axis=0)
+
+def mfccMinLL(textgrid, mfcc):
+    mfccTemp = _foundPattern(r'(.*)LL.', r'.Z', textgrid, mfcc)
+    return np.amin(mfccTemp, axis=0)
+
+
 
 def dummy(textgrid, mfcc):
     return '8'
