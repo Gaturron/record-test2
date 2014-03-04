6862ced8aff2cfe48b3bcabb55abfef30350d516
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2014-03-04 09:13:14 +0000

    paired tests

--- attributeExtractor/extract.py
+++ attributeExtractor/extract.py
@@ -1,11 +1,20 @@
+from __future__ import division
+
 import alignmentFilter as agmntFilter
+import diccToArff1 as dTA
 from textgridExtractor import textgridExtractor
 from acousticExtractor import acousticExtractor
-import diccToArff1 as dTA
 import logging
+import os
 
-Path = '/home/fernando/Tesis/Prosodylab-Aligner-master/data1/'
-logging.basicConfig(level=logging.DEBUG)
+from sets import Set
+from random import randint
+
+Path = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))+'/Prosodylab-Aligner-master/data1.complete/'
+PathTests = os.path.abspath(os.getcwd())+'/tests/'
+
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=None)
 
 def extract():
 
@@ -49,7 +58,7 @@ def extract():
     logger.info('starting')
 
     pathList = agmntFilter.filter(Path)
-    logger.debug('pathList: '+ str(pathList))
+    logger.debug('pathList: (tam. '+str(len(pathList))+') '+ str(pathList))
 
     tgExtractor = textgridExtractor(attributesFilter.keys())
     tgRes = tgExtractor.textgridsToAtt(pathList)
@@ -75,7 +84,67 @@ def extract():
 
     logger.debug('Res: '+ str(res1))
 
-    dTA.diccToArff(res1, 'testNew.arff', attributesFilter)
+    dTA.diccToArff(res1, PathTests+'testNew.arff', attributesFilter)
+
+    # Vamos a armar los casos de tests
+    print "Casos de tests:"
+    print "==============="
+
+    for j in range(10):
+        train = dict(res1)
+        test = {}
+        
+        usersBsas = list(Set([ v["userId"] for k, v in train.items() if v["place"] == "bsas" ]))
+        usersCba = list(Set([ v["userId"] for k, v in train.items() if v["place"] == "cba" ]))
+        print str(j)+") usersBsas: "+str(len(usersBsas))+" usersCba:"+str(len(usersCba))
+
+        while (len(test) / len(res1)) < 0.10:
+
+            if len(usersBsas) != 0:
+                inxBsas = randint(0, len(usersBsas) - 1)
+                userBsas = usersBsas.pop(inxBsas)
+
+                for k, v in train.items():
+                    if v["userId"] == userBsas:
+                        v1 = dict(v)
+
+                        v1.pop("userId", None)
+                        v1.pop("phraseId", None)
+                        v1.pop("attempt", None)
+                        v1.pop("phrases", None)
+                        
+                        test[k] = v1
+                        train.pop(k, None)
+
+            if len(usersCba) != 0:
+                inxCba = randint(0, len(usersCba) - 1)
+                userCba = usersCba.pop(inxCba)
+                
+                for k, v in train.items():
+                    if v["userId"] == userCba:
+                        v1 = dict(v)    
+
+                        v1.pop("userId", None)
+                        v1.pop("phraseId", None)
+                        v1.pop("attempt", None)
+                        v1.pop("phrases", None)
+
+                        test[k] = v1
+                        train.pop(k, None)
+
+            print " - "+str(len(train))+" "+str(len(test))+" - "+str(len(test) / len(res1))
+
+        print len(train)
+        print len(test)
+
+        attributesFilter1 = dict(attributesFilter)
+        attributesFilter1.pop("userId", None)
+        attributesFilter1.pop("phraseId", None)
+        attributesFilter1.pop("attempt", None)
+        attributesFilter1.pop("phrases", None)
+        attributesFilter1.pop("duration", None)
+        dTA.diccToArff(train, PathTests+'train'+str(j)+'.arff', attributesFilter1)
+        dTA.diccToArff(test, PathTests+'test'+str(j)+'.arff', attributesFilter1)
 
 if __name__ == '__main__':
     extract()
---
+++ wekaAnalisis/analize1.py
@@ -0,0 +1,116 @@
+import sys
+
+import java.io.FileReader as FileReader
+import java.lang.StringBuffer as StringBuffer
+import java.lang.StringBuffer as StringBuffer
+import java.lang.Boolean as Boolean
+import java.util.Random as Random
+
+import weka.core.Instances as Instances
+import weka.core.Utils as utils
+import weka.filters.Filter as Filter
+import weka.core.Range as Range
+
+import weka.classifiers.trees.J48 as J48
+import weka.classifiers.rules.ZeroR as ZeroR
+import weka.classifiers.rules.JRip as JRip
+import weka.classifiers.bayes.NaiveBayes as NaiveBayes
+import weka.classifiers.functions.SMO as functionsSMO
+
+import weka.filters.unsupervised.attribute.Remove as Remove
+import weka.filters.supervised.attribute.AttributeSelection as AttributeSelection
+
+import weka.classifiers.Evaluation as Evaluation
+
+import weka.classifiers.meta.AttributeSelectedClassifier as AttributeSelectedClassifier
+
+import weka.attributeSelection.CfsSubsetEval as CfsSubsetEval
+import weka.attributeSelection.InfoGainAttributeEval as InfoGainAttributeEval
+
+import weka.attributeSelection.GreedyStepwise as GreedyStepwise
+import weka.attributeSelection.Ranker as Ranker
+
+def printSummary(classifier, train, evaluation):
+    print "=== Run information ===\n"
+    print "Scheme: "+classifier.getClass().getName()+" "+utils.joinOptions(classifier.getOptions())
+    print "Relation: "+str(train.relationName())
+    print "Instances: "+str(train.numInstances())
+    print "Attributes: "+str(train.numAttributes())+"\n" 
+
+    print "=== Classifier model ===\n"
+    print classifier
+
+    print "=== Summary ==="
+    print evaluation.toSummaryString()
+    print evaluation.toClassDetailsString()
+    print evaluation.toMatrixString()
+
+def runZeroR(train, test):
+    zeroR = ZeroR()
+    zeroR.setDebug(True)
+
+    zeroR.buildClassifier(train)
+    evaluation = Evaluation(train)
+
+    evaluation.evaluateModel(zeroR, test)
+    printSummary(zeroR, train, evaluation)
+
+def runJRip(train, test):
+    jrip = JRip()
+    jrip.setDebug(True)
+    jrip.setOptions(['-F 3', '-N 2.0', '-O 2', '-S 1'])
+
+    jrip.buildClassifier(train)
+    evaluation = Evaluation(train)
+
+    evaluation.evaluateModel(jrip, test)
+    printSummary(jrip, train, evaluation)
+
+def runJ48(train, test):
+    j48 = J48()
+    j48.setDebug(True)
+
+    j48.buildClassifier(train)
+    evaluation = Evaluation(train)
+
+    evaluation.evaluateModel(j48, test)
+    printSummary(j48, train, evaluation)
+
+def runFunctSMO(train, test):
+    functSMO = functionsSMO()
+    functSMO.setDebug(True)
+
+    functSMO.buildClassifier(train)
+    evaluation = Evaluation(train)
+
+    evaluation.evaluateModel(functSMO, test)
+    printSummary(functSMO, train, evaluation)
+
+def runNaiveBayes(train, test):
+    naiveBayes = NaiveBayes()
+    naiveBayes.setDebug(True)
+
+    naiveBayes.buildClassifier(train)
+    evaluation = Evaluation(train)
+
+    evaluation.evaluateModel(naiveBayes, test)
+    printSummary(naiveBayes, train, evaluation)
+
+
+for i in range(10):
+
+    file = FileReader("/home/fernando/Tesis/record-test2/attributeExtractor/tests/train"+str(i)+".arff")
+    train = Instances(file)
+    train.setClassIndex(train.attribute('place').index())
+
+    file = FileReader("/home/fernando/Tesis/record-test2/attributeExtractor/tests/test"+str(i)+".arff")
+    test = Instances(file)
+    test.setClassIndex(test.attribute('place').index())
+
+    print "Corriendo paired wilcoxon tests: train"+str(i)+" test"+str(i)
+    print "====================================================================="
+    runZeroR(train, test)
+    runJRip(train, test)
+    runJ48(train, test)
+    runFunctSMO(train, test)
+    runNaiveBayes(train, test)
