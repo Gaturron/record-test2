ddbe6f8a52395720bafbfc526f416d25cf899afa
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2014-05-16 04:14:00 +0000

    pseudocodigo de testgenerate

--- attributeExtractor/testGenerator.py
+++ attributeExtractor/testGenerator.py
@@ -19,6 +19,24 @@ def generate(res1, PathTests, attributesFilter):
         usersCba = list(Set([ v["userId"] for k, v in train.items() if v["place"] == "cba" ]))
         print str(j)+") usersBsas: "+str(len(usersBsas))+" usersCba:"+str(len(usersCba))
 
+        # while there is people in res1
+        #    if bsas(train) / cba(train) < 60/40:
+        #       add(train, bsas)
+        #    else:
+        #       add(train, cba)
+
+        #    if bsas(test) / cba(test) < 60/40:
+        #       add(test, bsas)
+        #    else:
+        #       add(test, cba)
+
+        # (no es lo mismo agregar una persna que otra ojo)
+        # definir bien el desbalanceo, o sea si es sobre persona o audio
+
+        # if different( <train, test>, res)
+        #   add(<train, test>, res)
+        # else try again
+
         while (len(test) / len(res1)) < 0.10:
 
             if len(usersBsas) != 0:
