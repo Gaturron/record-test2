0b56ee926ac5a247f896f24d76623228134f23e9
fernando.bugni <fernando.bugni@e77a5664-c6a9-55b6-5474-8fd87dc5e844>
2014-12-12 13:09:45 +0000

    arreglos de correcciones

---
+++ attributeExtractor/testGeneratorUsingDicc_vLuciana_avgAttr.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from sets import Set
+
+import diccToArff1 as dTA
+
+import os
+import pickle
+import operator
+
+import multiprocessing
+import time
+import random
+import sys
+import types
+
+"""
+Idea: 
+Atributos:    A1    A2    A3         .....     AN
+H1:  audio1   1     ?     2                    2   
+     audio2   ?     ?     1                    ?
+     audio3   2     ?     3                    ?
+
+H2:  audio1   1     ?     ?                    ?
+     audio2   1     2     ?                    ?
+
+esto pasaría a:
+Atributos:    A1    A2    A3         .....     AN
+H1:  audio1   1.5   ?     1.667                2   
+H2:  audio1   1     2     ?                    ?
+"""
+
+path = os.path.abspath(os.getcwd())+'/tests/test_xHablante_avgAttr_correccionLuciana'
+dicc = pickle.load(open(path+"/extractionTotalDicc.p", "rb"))
+
+mfcc_len = 33
+attributesFilter = {
+    'place': ('bsas', 'cba'), 
+
+    'FON_kt_norm': 'NUMERIC', 
+    'FON_kt_normhd': 'NUMERIC',
+    'FON_ll_norm': 'NUMERIC', 
+    'FON_ll_normhd': 'NUMERIC', 
+    'FON_rr_norm': 'NUMERIC',  
+    'FON_rr_normhd': 'NUMERIC', 
+    'FON_sc_norm': 'NUMERIC',
+    'FON_sc_normhd': 'NUMERIC',
+    'FON_Sfinal_norm': 'NUMERIC',
+    'FON_Sfinal_normhd': 'NUMERIC',
+    
+    'FON_phoneme': 'NUMERIC',
+    'FON_vowel_norm': 'NUMERIC', 
+    'FON_vowel_normhd': 'NUMERIC', 
+    'FON_consonant_norm': 'NUMERIC',
+    'FON_consonant_normhd': 'NUMERIC',
+    
+    'SIL_syllableAccent_norm': 'NUMERIC',
+    'SIL_syllableAccent_normhd': 'NUMERIC',
+    'SIL_prevSyllableAccent_norm': 'NUMERIC',
+    'SIL_prevSyllableAccent_normhd': 'NUMERIC'
+}
+
+def resPairedToArff(resultsPaired, attributesFilter, folder):
+
+    """
+    La idea es tener 8 hablantes de Córdoba y 8 de Buenos Aires
+    y hacer los fold dejando 1 hab para test y los demás para train
+    """
+
+    if not os.path.exists(path+'/'+folder):
+        os.makedirs(path+'/'+folder)
+
+    i = 0
+    for (train, test) in resultsPaired:
+        dTA.diccToArff(train, path+'/'+folder+'/train'+str(i)+'.arff', attributesFilter)
+        dTA.diccToArff(test, path+'/'+folder+'/test'+str(i)+'.arff', attributesFilter)        
+        i = i+1
+
+def juntandoGrabaciones(dicc):
+    """
+    La idea es iterar en el dicc y si hay varias grabaciones de 1 hablante juntar 
+    todas las metricas en 1 grabacion
+    """
+    att_for_avg = [
+        'userId',
+        'place', 
+
+        'FON_kt_norm',
+        'FON_kt_normhd',
+        'FON_ll_norm',
+        'FON_ll_normhd',
+        'FON_rr_norm',
+        'FON_rr_normhd',
+        'FON_sc_norm',
+        'FON_sc_normhd',
+        'FON_Sfinal_norm',
+        'FON_Sfinal_normhd',
+        
+        'FON_phoneme',
+        'FON_vowel_norm',
+        'FON_vowel_normhd', 
+        'FON_consonant_norm',
+        'FON_consonant_normhd',
+
+        'SIL_syllableAccent_norm',
+        'SIL_syllableAccent_normhd',
+        'SIL_prevSyllableAccent_norm',
+        'SIL_prevSyllableAccent_normhd'
+    ]
+
+    print '1) ================================================='
+
+    dicc2 = {}
+    #ordenar por userId
+    for k, v in dicc.items():
+        userId = v['userId'] 
+        
+        if userId in dicc2:
+            dicc2[str(userId)] = [v] + dicc2[str(userId)] 
+        else:
+            dicc2[str(userId)] = [v]
+    print str(dicc2['24'])
+    print '2) =================================================='
+
+    dicc3 = {}
+    #recorro el diccionario juntando en cada atributo una lista con sus valores
+    for userId, audios in dicc2.items():
+        
+        dicc_temp = {}
+        for audio in audios:
+            for k, att in audio.items():
+                if k in att_for_avg and k in dicc_temp:
+                    dicc_temp[k] = dicc_temp[k] + [ att ] 
+                if k in att_for_avg and k not in dicc_temp:
+                    dicc_temp[k] = [ att ]
+
+        dicc3[userId] = dicc_temp
+    print str(dicc3['24'])
+    print '4) =================================================='
+
+    #recorro el diccionario haciendo el promedio de cada valor
+    def avg(l):
+        sum = 0
+        div = 0
+        
+        for x in l:
+            if x != None:
+                sum = sum + x
+                div = div + 1
+
+        if div != 0:
+            return sum / div
+        else:
+            return None
+
+    # IMPORTANTE
+    # Ojo: acá se va a iterar por los valores de los atributos de todos los audios
+    # si hay 2 audios: a1, a2. 
+    # Para el atributo FON_kt_norm a1 da 2 y en a1 NO SE CONTABILIZA (da None)
+    # se debería respetar ese valor y descartar el None
+
+    def allEqual(l):
+        elem = None
+        for e in l:
+            if e == elem:
+                elem = e
+            else:
+                return False
+
+    def allNone(l):
+        res = False
+        for e in l:
+            if e == None:
+                res = True
+        return res
+
+    dicc4 = {}
+    for userId, audios in dicc3.items():
+        
+        dicc_temp = {}
+        for k, att in audios.items():
+            if len(att) > 0:
+                if isinstance(att[0], basestring):
+                    dicc_temp[k] = att[0]
+                else:
+                    dicc_temp[k] = avg(att)
+
+        dicc4[userId] = dicc_temp
+    
+    print str(dicc4['24'])
+    print 'Fin  ==============================================='
+
+    return dicc4
+
+if __name__ == '__main__':
+
+    # promedio los atributos de los hablantes
+    dicc = juntandoGrabaciones(dicc)
+
+    # obtengo los usuarios
+    cantTotal = 27
+    cantCba = 0
+    cantBsAs = 0
+    
+    users = set([])
+    for k, v in dicc.items():
+
+        if v['place'] == 'cba' and v['userId'] not in users and cantCba < cantTotal:
+            cantCba = cantCba + 1
+            userId = v['userId']
+            users.add(userId)
+        
+        if v['place'] == 'bsas' and v['userId'] not in users and cantBsAs < cantTotal:
+            cantBsAs = cantBsAs + 1
+            userId = v['userId']
+            users.add(userId)
+
+    print "Cantidad de hablantes de Córdoba "+str(cantCba)     
+    print "Cantidad de hablantes de Buenos Aires "+str(cantBsAs)
+
+    # armamos los tests de la forma <train, test>
+    resultsPaired = []
+
+    print str(users)
+
+    for u in users:
+        test = dict((k,v) for k, v in dicc.items() if v["userId"] == u and v["userId"] in users)
+        train = dict((k,v) for k, v in dicc.items() if v["userId"] != u and v["userId"] in users)
+
+        print "test "+str(len([k for k, v in test.items()]))+" userId"+str([v["userId"] for k, v in test.items()])
+        print "train "+str(len([k for k, v in train.items()]))
+        print "----------------------------------------------"
+
+        resultsPaired = resultsPaired + [(train, test)]
+
+    resPairedToArff(resultsPaired, attributesFilter, "version1")
